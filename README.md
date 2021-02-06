a09e7d832107764f7c63428966a0bf5a7be8b72d
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2021-02-05 18:29:34 -0800

    Create initial outline of project with essential files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # wsd-485c
-Optimizing a Word Sense Disambiguation algorithm in C++ 
+
+## Overview
+
+This repository contains the implementation and optimization efforts of the
+Simplified Sense Word Disambiguation (WSD) algorithm written in C++ as part of
+UVic's CSC485C course project. This project belongs to:
+[Ahmed Siddiqui](emailto:jesuisahmedn@gmail.com) and
+[Jordan Kirchner](emailto:JordanKirchner04@hotmail.com)
+
+## Implementation
+
+The algorithm implementated is based on the simplified version of
+[Michael Lesk's algorithm](https://en.wikipedia.org/wiki/Lesk_algorithm#Simplified_Lesk_algorithm)
+devised in 1986.
---
+++ simplified_wsd.cpp
@@ -0,0 +1,28 @@
+/* 
+* Simplified Sense Word Disambiguation algorithm written by Ahmed Siddiqui and
+* Jordan Kirchner based off of Michael Lesk's simplified algorithm
+* (https://en.wikipedia.org/wiki/Lesk_algorithm#Simplified_Lesk_algorithm)
+*/
+
+/*
+
+Algorithm: 
+function SIMPLIFIED LESK(word,sentence) returns best sense of word
+    best-sense <- most frequent sense for word
+    max-overlap <- 0
+    context <- set of words in sentence
+    for each sense in senses of word do
+        signature <- set of words in the gloss and examples of sense
+        overlap <- COMPUTEOVERLAP (signature,context)
+        if overlap > max-overlap then
+            max-overlap <- overlap
+            best-sense <- sense
+end return (best-sense)
+
+*/
+
+string simplified_wsd(string word, string sentence) {
+
+    
+
+}
